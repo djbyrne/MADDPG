bac855e886ab2f443bc65b5a5a0249a5f9160c73
Donal Byrne <donaljbyrne@me.com>
2019-02-12 05:59:50 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,51 @@
 #!/usr/bin/env python
 
-from setuptools import setup, Command, find_packages
+from setuptools import setup, find_packages
+from os import path
+from io import open
 
+here = path.abspath(path.dirname(__file__))
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
-setup(name='unityagents',
-      version='0.4.0',
-      description='Unity Machine Learning Agents',
-      license='Apache License 2.0',
-      author='Unity Technologies',
-      author_email='ML-Agents@unity3d.com',
-      url='https://github.com/Unity-Technologies/ml-agents',
-      packages=find_packages(),
-      install_requires = required,
-      long_description= ("Unity Machine Learning Agents allows researchers and developers "
-       "to transform games and simulations created using the Unity Editor into environments "
-       "where intelligent agents can be trained using reinforcement learning, evolutionary " 
-       "strategies, or other machine learning methods through a simple to use Python API.")
-     )
+setup(
+    name='mlagents',
+    version='0.6.0',
+    description='Unity Machine Learning Agents',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Unity-Technologies/ml-agents',
+    author='Unity Technologies',
+    author_email='ML-Agents@unity3d.com',
+
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.6'
+    ],
+
+    packages=find_packages(exclude=['tests', 'tests.*', '*.tests', '*.tests.*']),  # Required
+
+    install_requires=[
+        'tensorflow>=1.7,<1.8',
+        'Pillow>=4.2.1',
+        'matplotlib',
+        'numpy>=1.13.3,<=1.14.5',
+        'jupyter',
+        'pytest>=3.2.2,<4.0.0',
+        'docopt',
+        'pyyaml',
+        'protobuf>=3.6,<3.7',
+        'grpcio>=1.11.0,<1.12.0'],
+
+    python_requires=">=3.6,<3.7",
+
+    entry_points={
+        'console_scripts': [
+            'mlagents-learn=mlagents.trainers.learn:main',
+        ],
+    },
+)
